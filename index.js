c4e9676d715c80468d13904f1eda805b11ae6f6c
wangjin <1934411016@qq.com>
2022-05-15 15:43:05 +0800

    提交

--- index.js
+++ index.js
@@ -7,12 +7,21 @@
 // ??表示当前文件未被追踪
 // A表示当前文件已经处于追踪文件了
 // M表示当前文件已经修改了
+// D表示已经被删除了
 // 添加追踪文件到暂存区：git add 要追踪的文件 -->  git add index.js
 // 其中可以先书写两个字母，然后按下tab键可以补全路径
 // 提交文件到仓库：git commit -m '描述信息'     命令后面的描述信息一定要写
 // 暂存已修改的文件 git add 要追踪的文件
 // 撤销上一步操作：git checkout -- 文件名
 // 一次性添加多个文件到暂存区：git add .
+// 跳过使用暂存区，直接将修改的文件存入git仓库：git commit -a -m '描述信息'   就是git add 和 git commit 的复合写法
+// 忽略文件： .gitignore
+// 查看提交的历史记录：git log
+// 回退到之前的指定版本：git reset --hard commitID
+
+
 // 取消单个暂存的文件：git reset HEAD 要移除的文件名  (了解)
 // 取消所有暂存的文件：git reset HEAD .   (了解)
-// 跳过使用暂存区，直接将修改的文件存入git仓库：git commit -a -m '描述信息'
+// 移除文件：git rm -f 要移除的文件(了解) (rm -> remove force强制)
+// 只移除仓库里面的文件，保留本地的文件：git rm --cached 要移除的文件
+
